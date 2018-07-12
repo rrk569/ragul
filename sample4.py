88b1dcda850257b91e449cad82e098ff5817cac8
rrk569 <41140449+rrk569@users.noreply.github.com>
2018-07-12 14:12:50 +0530

    Update sample4.py

--- sample4.py
+++ sample4.py
@@ -2,6 +2,6 @@ ch = input()
 if(ch=='b' and ch=='c' and ch =='d' and ch=='f' and ch=='g' and ch=='h' and ch=='j'
  and ch=='k' and ch=='l' and ch=='m' and ch=='n' and ch=='p' and ch=='q' and ch=='r' and ch =='s' and ch=='t'
  and ch=='v' and ch=='w' and ch =='x' and ch=='y' and ch=='z'):
-    print("Alphabet")
+    print("yes")
 else:
     print("NO")
