26ea06f61aacdf852ca4274a20ab3bd8d0020ce7
rrk569 <41140449+rrk569@users.noreply.github.com>
2018-07-12 14:05:04 +0530

    Update sample4.py

--- sample4.py
+++ sample4.py
@@ -1,7 +1,7 @@
 ch = input()
-if(ch=='b' or ch=='c' or ch =='d' or ch=='f' or ch=='g' or ch=='h' or ch=='j'
- or ch=='k' or ch=='l' or ch=='m' or ch=='n' or ch=='p' or ch=='q' or ch=='r' or ch =='s' or ch=='t'
- or ch=='v' or ch=='w' or ch =='x' or ch=='y' or ch=='z'):
+if(ch=='b' and ch=='c' and ch =='d' and ch=='f' and ch=='g' and ch=='h' and ch=='j'
+ and ch=='k' and ch=='l' and ch=='m' and ch=='n' and ch=='p' and ch=='q' and ch=='r' and ch =='s' and ch=='t'
+ and ch=='v' and ch=='w' or ch =='x' or ch=='y' or ch=='z'):
     print("Alphabet")
 else:
     print("NO")
